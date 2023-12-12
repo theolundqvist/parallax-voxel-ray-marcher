6fc77cbadb21cf699555b995655add07c035bcbb
Theodor Lundqvist <theodor.lundqvist@gmail.com>
2023-12-12 15:03:23 +0100

    shaders scene camera position

--- src/EDAN35/util/demo_scenes.cpp
+++ src/EDAN35/util/demo_scenes.cpp
@@ -25,12 +25,14 @@ typedef struct cam_pos_t {
 enum CAM {
     QUAD_CAM,
     CUBE_CAM,
+    SHADERS_CAM,
     MC_CAM,
 };
 
-cam_pos_t camera_positions[3] = {
+cam_pos_t camera_positions[] = {
         {glm::vec3(-0.15, -0.65, 6.5), glm::vec3(0, 0, 0)},
         {glm::vec3(4., 3.35, -5.5),    glm::vec3(0)},
+        {glm::vec3(2.0, 1.3, -3),    glm::vec3(0, -0.5, 0)},
         {glm::vec3(-1.5, 4.8, -7.5),    glm::vec3(-1.5, 2, 0)},
 };
 
@@ -52,7 +54,7 @@ scene_settings_t scenes[] = {
         {0, "Quad fixed",     camera_positions[QUAD_CAM],         4,  fixed_step_material},
         {1, "Cube fixed",     camera_positions[CUBE_CAM],         5,  fixed_step_material},
         {2, "Cube FVTA step", camera_positions[CUBE_CAM],         3,  fixed_step_material},
-        {3, "Shaders",        camera_positions[CUBE_CAM],         3,  fvta_step_material},
+        {3, "Shaders",        camera_positions[SHADERS_CAM],         NBR_SHADER_SETTINGS-1,  fvta_step_material},
         {4, "Larger",         camera_positions[CUBE_CAM],         1,  fvta_step_shaded_with_simple_AO, 128, 1,  true},
         {5, "SDF",            camera_positions[CUBE_CAM],         4,  fvta_step_shaded_with_simple_AO, 128, 1,  true},
         {6, "CA",             camera_positions[CUBE_CAM],         4,  fvta_step_shaded_with_simple_AO, 128, 1,  true},
